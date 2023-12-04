c7639a259eaabf668000814234c0c38e90d7cb67
FukeKazki <kazkichi0906@gmail.com>
2023-12-05 00:56:28 +0900

    Update

--- prettierrc.mjs
+++ prettierrc.mjs
@@ -1,5 +1,10 @@
 /** @type {import('prettier').Config} */
 export default {
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+
   plugins: ["prettier-plugin-astro"],
   overrides: [
     {
